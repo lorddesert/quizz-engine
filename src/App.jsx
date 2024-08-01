9b49339155fea12d28902c52e0387c238b44405f
lorddesert <sacha.nocetto@hotmail.com>
2024-08-01 16:51:39 -0300

    Add initial skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-const MOCK_DATA = {
-  title: 'Sacha super quizz app',
-  description: '', //???
-  question: 'Como se le dice en Argentina coloquialmente al automovil? ',
-  maxChoicesAmount: 1,
-
-}
-
-
-// This will come from the server.
-const choicesList = {
-  id: '',
-  text: '',
-  choices: [
-    'Carro',
-    'Auto',
-    'Coche',
-    'Caballo'
-  ],
-  // The answer will be the correct string, it needs to makes sense to the user that he's not choosing A or B, but
-  // it's content. Therefore, can be in _any_ given order. 
-  correctChoice: 'Auto', 
-  nextQuestion: {
-    correct: '',
-    incorrect: ''
-  }, //Reference ID of the next question, if it's undefined, then it's the last question
-}
+import QUIZ_SET from './mock-data/one-choice.json'
 
 function App() {
-  const [choicesProgress, setChoicesProgress] = useState(0)
-  const { title, description, choices, correctChoice, maxChoicesAmount } = MOCK_DATA
+  const questions = QUIZ_SET
+  const [selectedQuestionIndex, setSelectedQuestionIndex] = useState(0)
+  const { title, choices, description, image } = questions[selectedQuestionIndex]
 
+  const gamemode = ''
   return (
     <>
-      <h1>{title}</h1>
-      <form action="">
-        <progress max={maxChoicesAmount} value={choicesProgress} />
-        <ul>
-          {choices.map((choice, i) => <li key={`choice-${i}`}>
-            <label htmlFor={`choice-${i}`}>{choice}</label>
-            <input type="radio" name={`choice-${i}`} id={`choice-${i}`} />
-          </li>)}
-
+      <h1>Quizzork (Quizz engine)</h1>
+      <div className="card">
+        <ul className='gamemode-selector'>
+          <li className="card"><a href="/one-choice">One choice</a></li>
+          <li className="card"><a href="/multiple-choice">Multiple choice</a></li>
+          <li className="card"><a href="/input-choice">Input choice</a></li>
         </ul>
-        <legend>{description}</legend>
-        <button>Next</button>
-      </form>
+        <form onSubmit={e => {
+          e.preventDefault()
+        }}>
+          <input type="text" />
+          <button>Next</button>
+          <h3>{title}</h3>
+          <figure>
+            <img src={image} alt="Image for the question" />
+          </figure>
+          <legend>{description}</legend>
+          <progress max={questions.length} value={selectedQuestionIndex + 1} />
+          <ul>
+            {choices.map(choice => <li className='card' key={`choice-${choice}`}>
+              <label htmlFor={`choice-${choice}`}>
+                <input type="radio" name="choice" id={`choice-${choice}`} />
+                {choice}
+              </label>
+            </li>)}
+          </ul>
+          <button onClick={e => {
+            if (selectedQuestionIndex <= 0) return
+            setSelectedQuestionIndex(selectedQuestionIndex - 1)
+          }}>Previous question</button>
+          <button onClick={e => {
+            if (selectedQuestionIndex === questions.length - 1) return
+            setSelectedQuestionIndex(selectedQuestionIndex + 1)
+          }}>Next question</button>
+        </form>
+      </div>
     </>
+
   )
 }
 
