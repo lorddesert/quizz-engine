50f51f9131acd79a1faa8f3ee4e410b33fc1b79b
lorddesert <sacha.nocetto@hotmail.com>
2024-08-04 20:19:51 -0300

    Add score, change next question function

--- src/components/quizz-form.jsx
+++ src/components/quizz-form.jsx
@@ -6,35 +6,63 @@ import Choices from './choices'
 
 export default function QuizzForm() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
-  // const [correctChoice, setCorrectChoice] = useState(questionsJSON[0].correctChoice)
+  // const [correctChoice, setCor ectChoice] = useState(questionsJSON[0].correctChoice)
   const [choiceSelected, setChoiceSelected] = useState(false)
+  const [score, setScore] = useState(0)
+
 
   function nextQuestion() {
     if (currentQuestion === questionsJSON.length - 1) return
 
+    const form = document.querySelector('form')
+
+    document.querySelector('.correct').classList.remove('correct')
+    document.querySelector('.incorrect')?.classList?.remove('incorrect')
+
+    form.reset()
+
+    setChoiceSelected(false)
     setCurrentQuestion(currentQuestion + 1)
   }
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log(e)
   }
 
   const { title, description, choices, correctChoice } = questionsJSON[currentQuestion]
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="questions-container card p-0">
+    <form onSubmit={handleSubmit} className="card">
+      <div className="questions-container">
         <div className="question">
+          <progress max={questionsJSON.length} />
+          <span className="text-center">Score: {score}</span>
           <h4>{title}</h4>
           <fieldset>
+            <Choices
+              {...{
+                choices,
+                correctChoice,
+                setChoiceSelected,
+                choiceSelected,
+                setScore,
+                score
+              }}
+            />
             <legend>{description}</legend>
-
-            <Choices choices={choices} questionIndex={i} correctChoice={correctChoice} />
           </fieldset>
         </div>
       </div>
-      <button disabled={currentQuestion === questionsJSON.length - 1 || !choiceSelected} onClick={nextQuestion}>Next</button>
+      <button disabled={
+        currentQuestion === questionsJSON.length - 1
+        || !choiceSelected
+      } onClick={nextQuestion}
+        style={{
+          width: '100%'
+        }}
+      >
+        Next
+      </button>
     </form>
   )
 }
