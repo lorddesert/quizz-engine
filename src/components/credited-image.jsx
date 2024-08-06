d85a59c473c1195231f67b689d3f2d6ce90015e7
lorddesert <sacha.nocetto@hotmail.com>
2024-08-05 22:47:21 -0300

    Add a depency so that it re-renders the image for demo purposes

--- src/components/credited-image.jsx
+++ src/components/credited-image.jsx
@@ -3,26 +3,24 @@ import { useEffect, useState } from "react";
 // Components
 import SkeletonCreditedImage from "./skeleton-credited-image";
 
-export default function CreditedImage() {
+export default function CreditedImage({ id }) {
   const [photo, setPhoto] = useState(null)
   const imageWidth = 450
   const imageHeight = 150
 
   useEffect(() => {
-    if (photo) return
-
-      fetch(`https://picsum.photos/${imageWidth}/${imageHeight}`)
+    fetch(`https://picsum.photos/${imageWidth}/${imageHeight}`)
       .then(res => {
         setPhoto(res.url)
       })
-  }, [])
+  }, [id])
   return (
     <>
-      {photo 
-      ? <div className="question-photo-container">
-        <img src={photo} alt="Random image" />
-      </div>
-      : <SkeletonCreditedImage />}
+      {photo
+        ? <div className="question-photo-container">
+          <img src={photo} alt="Random image" />
+        </div>
+        : <SkeletonCreditedImage />}
     </>
   )
 }
