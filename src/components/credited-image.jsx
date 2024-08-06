18f00841b1f96cbfb3b92d4b703b15d6d6255265
lorddesert <sacha.nocetto@hotmail.com>
2024-08-05 22:31:31 -0300

    Add random image with external API

--- src/components/credited-image.jsx
+++ src/components/credited-image.jsx
@@ -1,23 +1,28 @@
-import { Suspense } from "react";
+import { useEffect, useState } from "react";
 
 // Components
 import SkeletonCreditedImage from "./skeleton-credited-image";
 
-export default function CreditedImage({ user, urls }) {
+export default function CreditedImage() {
+  const [photo, setPhoto] = useState(null)
+  const imageWidth = 450
+  const imageHeight = 150
+
+  useEffect(() => {
+    if (photo) return
+
+      fetch(`https://picsum.photos/${imageWidth}/${imageHeight}`)
+      .then(res => {
+        setPhoto(res.url)
+      })
+  }, [])
   return (
-    <Suspense fallback={SkeletonCreditedImage}>
-      <picture>
-      {/* <img className="img" src={urls.regular} />
-      <caption>
-        <a
-          className="credit"
-          target="_blank"
-          href={`https://unsplash.com/@${user.username}`}
-        >
-          {user.name}
-        </a>
-      </caption> */}
-      </picture>
-    </Suspense>
+    <>
+      {photo 
+      ? <div className="question-photo-container">
+        <img className="photo" src={photo} alt="Random image" />
+      </div>
+      : <SkeletonCreditedImage />}
+    </>
   )
 }
